d12aa0cae2578e74ee94ee2a91e1242c786ad7d7
Shlomi Fish <shlomif@shlomifish.org>
2016-02-15 14:09:35 +0200

    initial version of the module - non functional.

---
+++ lib/Test/HTML/Tidy/Recursive.pm
@@ -0,0 +1,17 @@
+package Test::HTML::Tidy::Recursive;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.0.1';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Test::HTML::Tidy::Recursive - recursively check files in a directory using
+HTML::Tidy .
+
+=cut
